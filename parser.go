9d3cc380bd5bda3208375f1006f9b7b7c96feced
Wellington Gadelha <contato.informeai@gmail.com>
2021-07-31 10:02:50 -0300

    add some tests.

--- parser.go
+++ parser.go
@@ -1,1 +1,10 @@
 package markhub
+
+//MarkHub is struct base for manipulate and parse,
+//text the files markdown.
+type MarkHub struct{}
+
+//NewMarkHub return of instance the MarkHub
+func NewMarkHub() *MarkHub {
+	return &MarkHub{}
+}
--- parser_test.go
+++ parser_test.go
@@ -1,1 +1,34 @@
 package markhub
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+// go test -v -run ^TestNewMarkHub
+func TestNewMarkHub(t *testing.T) {
+	m := NewMarkHub()
+	if m == nil {
+		t.Errorf("TestNewMarkHub(): got -> nil, want: MarkHub{}")
+	}
+}
+
+// go test -v -run ^TestReadFile
+func TestReadFile(t *testing.T) {
+	b, err := ioutil.ReadFile("./test/tester.md")
+	if err != nil {
+		t.Errorf("TestReadFile(): got -> %v, want: []byte ", err)
+	}
+	fmt.Println(string(b))
+
+}
+
+// go test -v -run ^TestReadString
+func TestReadString(t *testing.T) {
+	reader := strings.NewReader("# title 1")
+	if reader.Len() == 0 {
+		t.Errorf("TestReadString(): got -> %v, want: lenght > 0", reader)
+	}
+}
